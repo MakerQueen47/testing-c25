3475a85ff5f9ff100093f2afa4a7dd0b81a38308
Silvia <Silvia.thomas47@gmail.com>
2020-10-21 23:29:30 +0530

    aadded platform

--- Ground.js
+++ Ground.js
@@ -10,6 +10,7 @@ class Ground {
     }
     display(){
       var pos =this.body.position;
+      strokeWeight(0);
       rectMode(CENTER);
       fill("brown");
       rect(pos.x, pos.y, this.width, this.height);
